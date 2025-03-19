62b6f453dd4864aabd06e60b51c738886925c83b
giovanni.maffeo <giovanni.maffeo@gmail.com>
2025-03-19 21:51:26 +0000

    feat: menu options main

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import preProcess
 import utils
+import preProcess
+import featureSelectionReduction
 import classifiers
 
 def main():
     dfData, dfLabels = preProcess.preProcessDataset()
-    dfData = preProcess.featureSelectionPCA(dfData)
-    dfData = preProcess.featureSelectionLDA(dfData, dfLabels)
-    #dfTrain, dfTest, dfTargetTrain, dfTargetTest = utils.train_test_split(dfData, dfLabels, test_size=0.3, random_state=42)
-    # classifiers.fisherLDA(dfTrain, dfTest, dfTargetTrain, dfTargetTest)
+    featureReductionSelection, classifierSelection = utils.showMenu()
+
+    optionsFeatureReductionSelection = {
+        1: featureSelectionReduction.featureReductionPCA,
+        2: featureSelectionReduction.featureReductionLDA,
+        3: featureSelectionReduction.featureSelectionKsTest
+    }
+    
+    dfData = optionsFeatureReductionSelection[featureReductionSelection](dfData, dfLabels)
+    dfTrain, dfTest, dfTargetTrain, dfTargetTest = utils.train_test_split(dfData, dfLabels, test_size=0.3, random_state=42)
+
+    optionsFeatureClassifier = {
+        1: classifiers.fisherLDA,
+        2: classifiers.eucludianMinimumDistanceClassifier,
+        3: classifiers.mahalanobisMinimumDistanceClassifier
+    }
+    optionsFeatureClassifier[classifierSelection](dfTrain, dfTest, dfTargetTrain, dfTargetTest)
     
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 from sklearn.decomposition import PCA
@@ -7,3 +8,19 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 from scipy import stats
+
+
+def showMenu():
+    print("Please choose an option for feature reduction / selection: ")
+    print("[1] - PCA")
+    print("[2] - LDA")
+    print("[3] - KS")
+    featureReductionSelection = int(input("Option -> "))
+
+    print("Please choose an option for classification: ")
+    print("[1] - Fisher LDA")
+    print("[2] - Euclidian Minimum Distance Classifier")
+    print("[3] - Mahalanobis Minimum Distance Classifier")
+    classifierSelection = int(input("Option -> "))
+
+    return featureReductionSelection, classifierSelection
